a33e39568a58b4466187808683dc1c8e0b103ba2
XiongQi <xiongqi.me@foxmail.com>
2018-05-11 23:56:11 +0800

    Feat(Classroom): finish Classroom v1.0

--- src/client/page/course/Course.jsx
+++ src/client/page/course/Course.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { Table } from 'antd';
+import { Table, message } from 'antd';
 import ReactDOM from 'react-dom';
+import Header from 'components/Header';
 import { fetchJSON } from '../../utils/fetchFunc';
 import { ApiBase } from '../../utils/common';
 import './course.scss';
+
+const Frag = React.Fragment;
 
 class Course extends React.Component {
   state = {
@@ -17,159 +20,137 @@ class Course extends React.Component {
   }
   componentDidMount() {
     const that = this;
+    const loaded = message.loading('获取课程信息 ...', 0);
+    this.handleFixedTable();
     fetchJSON(`${ApiBase}/course`, {
       method: 'POST',
       body: '',
       credentials: 'include',
-    }).then(res => that.setState({ course: res }));
+    }).then(res => that.setState({ course: res }, loaded));
   }
   formatCourse = course => {
     const arr = [];
     for (let i = 0; i < 13; i++) arr[i] = {};
     course.forEach(x => {
-      // const temp = {};
-      x.course_type.forEach(y => {
-        arr[y - 1][`week${x.week}`] = x;
-        // arr[y - 1] = Object.assign({}, arr[y - 1], temp);
-      });
+      if (x.course_type.length > 0) {
+        if (x.course_type.length === 1) arr[x.course_type[0] - 1][`week${x.week}`] = x;
+        else for (let j = Number(x.course_type[0] - 1); j < Number(x.course_type[1]); ++j) arr[j][`week${x.week}`] = x;
+      }
     });
+    // console.log('course:', course, '\nformat:', arr);
     return arr;
   };
+  handleFixedTable = () => {
+    const table = document.getElementById('fixedTable');
+    window.onscroll = () => {
+      if (window.pageYOffset > 50) table.style.visibility = 'visible';
+      else table.style.visibility = 'hidden';
+    };
+  };
+  handleCourseClick = text => {
+    message.info(
+      <div>
+        <p>课程：{text.course_name}</p>
+        <p>教室：{`${text.campus}${text.build} ${text.classroom}`}</p>
+        <p>教师：{text.teacher}</p>
+        <p>节次：{`${text.week_type} 星期${text.week} 第${text.course_type}小节`}</p>
+      </div>,
+      5
+    );
+  };
+  renderCourse = (text, index) => {
+    if (!text) return '';
+    const {
+      course_type: section, week_type: week, campus, build, course_name: name, classroom
+    } = text;
+    if (index + 1 !== Number(section[0])) return { children: '', props: { rowSpan: 0 } };
+    return {
+      children: (
+        <Frag>
+          <div>{name}</div>
+          <div>{`${campus}${build} ${classroom}`}</div>
+        </Frag>
+      ),
+      props: {
+        rowSpan: section.length === 1 ? 1 : section[1] - section[0] + 1,
+        className: `course${Math.floor(Math.random() * 7)}`,
+        onClick: () => this.handleCourseClick(text),
+      },
+    };
+  };
+  renderColConf = (name, week) => ({
+    title: name,
+    dataIndex: week,
+    width: 100,
+    render: (text, record, index) => this.renderCourse(text, index),
+  });
+
   render() {
     const { course } = this.state;
-    console.log(course, this.formatCourse(course));
     const columnsConf = [
       {
-        title: '',
-        key: 'time',
-        // colSpan: 2,
-        width: 40,
-        className: 'timeCol',
-        render: (text, record, index) => {
-          if (index === 0) return { children: '上午', props: { rowSpan: 4 } };
-          else if (index === 4) return { children: '中午', props: { rowSpan: 5 } };
-          else if (index === 9) return { children: '晚上', props: { rowSpan: 4 } };
-          return { children: '晚上', props: { rowSpan: 0 } };
-        },
+        title: '节次',
+        key: 'section',
+        width: 50,
+        className: 'section',
+        render: (text, record, index) => `${index + 1}`,
       },
-      {
-        title: '',
-        key: 'course',
-        // colSpan: 0,
-        width: 100,
-        render: (text, record, index) => `第${index + 1}节`,
-      },
-      {
-        title: '星期一',
-        dataIndex: 'week1.course_name',
-        width: 100,
-        render: (text, record) => {
-          if (text) {
-            const {
-              campus, build, course_name: name, classroom
-            } = record.week1;
-            return `${name}\n（${campus}${build} ${classroom}）`;
-          }
-          return '';
-        },
-      },
-      {
-        title: '星期二',
-        dataIndex: 'week2.course_name',
-        width: 100,
-        render: (text, record) => {
-          if (text) {
-            const {
-              campus, build, course_name: name, classroom
-            } = record.week2;
-            return `${name}\n（${campus}${build} ${classroom}）`;
-          }
-          return '';
-        },
-      },
-      {
-        title: '星期三',
-        dataIndex: 'week3.course_name',
-        width: 100,
-        render: (text, record) => {
-          if (text) {
-            const {
-              campus, build, course_name: name, classroom
-            } = record.week3;
-            return `${name}\n（${campus}${build} ${classroom}）`;
-          }
-          return '';
-        },
-      },
-      {
-        title: '星期四',
-        dataIndex: 'week4.course_name',
-        width: 100,
-        render: (text, record) => {
-          if (text) {
-            const {
-              campus, build, course_name: name, classroom
-            } = record.week4;
-            return `${name}\n（${campus}${build} ${classroom}）`;
-          }
-          return '';
-        },
-      },
-      {
-        title: '星期五',
-        dataIndex: 'week5.course_name',
-        width: 100,
-        render: (text, record) => {
-          if (text) {
-            const {
-              campus, build, course_name: name, classroom
-            } = record.week5;
-            return `${name}\n（${campus}${build} ${classroom}）`;
-          }
-          return '';
-        },
-      },
-      {
-        title: '星期六',
-        dataIndex: 'week6.course_name',
-        width: 100,
-        render: (text, record) => {
-          if (text) {
-            const {
-              campus, build, course_name: name, classroom
-            } = record.week6;
-            return `${name}\n（${campus}${build} ${classroom}）`;
-          }
-          return '';
-        },
-      },
-      {
-        title: '星期天',
-        dataIndex: 'week7.course_name',
-        width: 100,
-        render: (text, record) => {
-          if (text) {
-            const {
-              campus, build, course_name: name, classroom
-            } = record.week7;
-            return `${name}\n（${campus}${build} ${classroom}）`;
-          }
-          return '';
-        },
-      },
+      this.renderColConf('周一', 'week1'),
+      this.renderColConf('周二', 'week2'),
+      this.renderColConf('周三', 'week3'),
+      this.renderColConf('周四', 'week4'),
+      this.renderColConf('周五', 'week5'),
+      this.renderColConf('周六', 'week6'),
+      this.renderColConf('周日', 'week7'),
     ];
     const tableConf = {
       dataSource: this.formatCourse(course),
       bordered: true,
       columns: columnsConf,
       pagination: false,
-      // className: 'table',
-      // rowKey: record => record.course_id,
-      // rowSelection: {},
+      rowKey: (record, index) => index,
+    };
+    const headerConf = {
+      title: '课程表',
+      color: '#9652bf',
+      home: true,
     };
     return (
-      <div className="container">
-        <Table {...tableConf} />
+      <div>
+        <Header {...headerConf} />
+        <div className="container">
+          <table id="fixedTable">
+            <thead>
+              <tr>
+                <th>
+                  <span>节次</span>
+                </th>
+                <th>
+                  <span>周一</span>
+                </th>
+                <th>
+                  <span>周二</span>
+                </th>
+                <th>
+                  <span>周三</span>
+                </th>
+                <th>
+                  <span>周四</span>
+                </th>
+                <th>
+                  <span>周五</span>
+                </th>
+                <th>
+                  <span>周六</span>
+                </th>
+                <th>
+                  <span>周日</span>
+                </th>
+              </tr>
+            </thead>
+          </table>
+          <Table {...tableConf} />
+        </div>
       </div>
     );
   }
