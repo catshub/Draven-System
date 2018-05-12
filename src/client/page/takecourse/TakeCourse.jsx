a60d0d1f5d6cc46a1a603f1b555f49c46725afc4
XiongQi <xiongqi.me@foxmail.com>
2018-05-12 23:55:52 +0800

    Feat(TakeCourse): finish takecourse

--- src/client/page/takecourse/TakeCourse.jsx
+++ src/client/page/takecourse/TakeCourse.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Button, Form, Input, message, Row } from 'antd';
+import Header from 'components/Header';
+import scupng from 'img/scu.png';
+import './takecourse.scss';
 
 @Form.create()
 export default class TakeCourse extends React.Component {
@@ -60,24 +63,34 @@ export default class TakeCourse extends React.Component {
   render() {
     const { name } = sessionStorage.getItem('name');
     const { getFieldDecorator: GD } = this.props.form;
-    const itemLayout = { labelCol: { span: 6 }, wrapperCol: { span: 10 }, style: { textAlign: 'center' } };
+    const itemLayout = { /* labelCol: { xs: 2 }, */ wrapperCol: { span: 12 }, className: 'item' };
+    // const itemLayout = { labelCol: { span: 6 }, wrapperCol: { span: 10 }, style: { textAlign: 'center' } };
+    const headerConf = {
+      left: '抢课',
+      middle: '开放选课时可用',
+      color: '#9652bf',
+    };
     return (
-      <Row type="flex" justify="center">
-        <Form onSubmit={this.handleSubmit} style={{ margin: '10px', width: '800px' }}>
-          <Form.Item style={{ textAlign: 'center' }}>
-            <span>{name}</span>
-          </Form.Item>
-          <Form.Item label="课程号" {...itemLayout}>
-            {GD('kch', { initialValue: '', rules: [{ required: true, message: '请输入数字课程号!' }] })(<Input placeholder="如: 909019020" />)}
-          </Form.Item>
-          <Form.Item label="课序号" {...itemLayout}>
-            {GD('cxkxh', { initialValue: '', rules: [{ required: true, message: '请输入数字课序号!' }] })(<Input placeholder="如: 01" />)}
-          </Form.Item>
-          <Form.Item style={{ textAlign: 'center' }}>
-            <Button htmlType="submit">抢课</Button>
-          </Form.Item>
-        </Form>
-      </Row>
+      <div>
+        <Header {...headerConf} />
+        <Row type="flex" justify="center">
+          <Form onSubmit={this.handleSubmit} className="form">
+            <img src={scupng} alt="" />
+            <Form.Item>
+              <span>{name}</span>
+            </Form.Item>
+            <Form.Item label="课程号" {...itemLayout}>
+              {GD('kch', { initialValue: '', rules: [{ required: true, message: '请输入数字课程号!' }] })(<Input placeholder="如: 909019020" />)}
+            </Form.Item>
+            <Form.Item label="课序号" {...itemLayout}>
+              {GD('cxkxh', { initialValue: '', rules: [{ required: true, message: '请输入数字课序号!' }] })(<Input placeholder="如: 01" />)}
+            </Form.Item>
+            <Form.Item>
+              <Button htmlType="submit">抢课</Button>
+            </Form.Item>
+          </Form>
+        </Row>
+      </div>
     );
   }
 }
