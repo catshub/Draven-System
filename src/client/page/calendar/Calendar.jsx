95d4d81731e8cdaab2d472aa5f98a8a2b2091df3
XiongQi <xiongqi.me@foxmail.com>
2018-05-16 20:41:47 +0800

    Feat: finish Calendar

--- src/client/page/calendar/Calendar.jsx
+++ src/client/page/calendar/Calendar.jsx
@@ -1,79 +1,67 @@
 import React from 'react';
-import { Table, message } from 'antd';
+import { Select, message } from 'antd';
 import ReactDOM from 'react-dom';
-import { fetchJSON } from '../../utils/fetchFunc';
-import { ApiBase } from '../../utils/common';
+import Header from 'components/Header';
+import { fetchJSON } from 'utils/fetchFunc';
+import { ApiBase } from 'utils/common';
 import './calendar.scss';
 
 class Course extends React.Component {
   state = {
-    calendar: [],
+    list: [],
+    now: [],
+    listHtml: {},
   };
   componentWillMount() {
     const that = this;
     const loading = message.loading('...', 0);
-    fetchJSON(`${ApiBase}/calendar`).then(res => that.setState({ calendar: res }, loading));
+    fetchJSON(`${ApiBase}/calendar`).then(res => that.setState({ list: res.list, now: res.now }, loading));
   }
+  handleXlClick = value => {
+    const { key, href } = JSON.parse(value);
+    const { listHtml } = this.state;
+    if (listHtml[key]) this.setState({ now: listHtml[key] });
+    else {
+      const that = this;
+      const loaded = message.loading('获取校历信息...', 0);
+      fetchJSON(`${ApiBase}/calendar`, { method: 'POST', body: href }).then(res => {
+        that.setState({ listHtml: Object.assign({}, listHtml, { [key]: res.now }), now: res.now }, loaded);
+      });
+    }
+  };
   renderTable = tableStr => {
     const div = document.createElement('div');
     div.innerHTML = tableStr;
     return <div />;
   };
   render() {
-    const { calendar } = this.state;
-    const columnsConf = [
-      {
-        title: (calendar[0] && calendar[0].busCol1) || '',
-        dataIndex: 'busCol1',
-        width: 100,
-        render: (text, record, index) => {
-          if (index === 0) return { children: '1', props: { rowSpan: 0 } };
-          return text;
-        },
-      },
-      {
-        title: (calendar[0] && calendar[0].busCol2) || '',
-        dataIndex: 'busCol2',
-        width: 100,
-        render: (text, record, index) => {
-          if (index === 0) return { children: '1', props: { rowSpan: 0 } };
-          return text;
-        },
-      },
-      {
-        title: (calendar[0] && calendar[0].busCol3) || '',
-        dataIndex: 'busCol3',
-        width: 100,
-        render: (text, record, index) => {
-          if (index === 0) return { children: '1', props: { rowSpan: 0 } };
-          return text;
-        },
-      },
-      {
-        title: (calendar[0] && calendar[0].busCol4) || '',
-        dataIndex: 'busCol4',
-        width: 100,
-        render: (text, record, index) => {
-          if (index === 0) return { children: '1', props: { rowSpan: 0 } };
-          return text;
-        },
-      },
-    ];
-    const tableConf = {
-      dataSource: calendar,
-      bordered: true,
-      columns: columnsConf,
-      pagination: false,
-      // showHeader: false,
+    const { now = [], list = [] } = this.state;
+    const headerConf = {
+      color: '#eca403',
+      left: '校历',
+      middle: (
+        <Select
+          // defaultValue={editData.name}
+          className="selector"
+          showArrow={false}
+          onChange={value => this.handleXlClick(value)}
+          dropdownMatchSelectWidth={false}
+          placeholder="本学年校历"
+        >
+          {list.map(x => (
+            <Select.Option key={x.key} value={JSON.stringify({ href: x.href, key: x.key })}>
+              {x.name}
+            </Select.Option>
+          ))}
+        </Select>
+      ),
     };
     return (
-      <div className="container">
-        {calendar.map(x => (
-          <div>
-            <p>{x.type}</p>
-            <div className="table" dangerouslySetInnerHTML={{ __html: x.table }} />
-          </div>
-        ))}
+      <div>
+        <Header {...headerConf} />
+        <div className="container">
+          {now.map(x => <div key={x.key} className="tableWrapper" dangerouslySetInnerHTML={{ __html: x.table }} />)}
+        </div>
       </div>
     );
   }
--- src/server/function/calendarFunc.js
+++ src/server/function/calendarFunc.js
@@ -4,33 +4,78 @@ const cheerio = require('cheerio');
 
 function formatCalendar(scuData) {
   const $ = cheerio.load(scuData);
-  const table = $('table').removeAttr('class').removeAttr('style');
+  const table = $('table');
+  // .removeAttr('class')
+  // .removeAttr('style');
   const data = [];
   table.each((i, el) => {
+    const key = cheerio(el)
+      .prev()
+      .text()
+      .trim();
+    const thead = `<thead><tr><th colspan="100">${key}</tr></th></thead>`;
     data.push({
-      type: cheerio(el)
-        .prev()
-        .text()
-        .trim(),
-      table: $.html(el),
+      key,
+      table: `<table border="1" class="table${key.substring(0, 4)}">${thead}${$(el).html()}</table>`,
     });
   });
   return data;
 }
 
+function formatAllCal(scuData) {
+  const $ = cheerio.load(scuData);
+  const as = $('#serach_div .list-a-content .list-b-right ul li a');
+  const data = [];
+  as.each((i, el) => {
+    const a = cheerio(el);
+    data.push({
+      name: a.text(),
+      href: a.attr('href'),
+      key: a.text().substring(0, 4),
+    });
+  });
+  return data;
+}
+function getAllCal() {
+  return new Promise(reslove => {
+    const scuReq = Http.request('http://jwc.scu.edu.cn/article/206/206_1.htm', response => {
+      let scuData = '';
+      response.on('data', d => {
+        scuData += d;
+      });
+      response.on('end', () => {
+        const res = formatAllCal(scuData);
+        reslove(res);
+      });
+    });
+    scuReq.end();
+  });
+}
 // classroom
-function calendarFunc(option, res, origin) {
-  const scuReq = Http.request(option, response => {
+function calendarFunc(data = '', option, res, origin) {
+  const scuReq = Http.request(data || option, response => {
     let scuData = '';
     response.on('data', d => {
       scuData += d;
     });
     response.on('end', () => {
-      res.writeHead(200, {
-        'Access-Control-Allow-Origin': origin,
-      });
-      res.end(JSON.stringify(formatCalendar(scuData)));
-      Log.Tip('name(): Calendar');
+      if (!data) {
+        getAllCal().then(list => {
+          res.writeHead(200, {
+            'Access-Control-Allow-Origin': origin,
+          });
+          const now = formatCalendar(scuData);
+          res.end(JSON.stringify({ list, now }));
+          Log.Tip('name(): Calendar');
+        });
+      } else {
+        res.writeHead(200, {
+          'Access-Control-Allow-Origin': origin,
+        });
+        const now = formatCalendar(scuData);
+        res.end(JSON.stringify({ now }));
+        Log.Tip('name(): Calendar');
+      }
     });
   });
   scuReq.end();
