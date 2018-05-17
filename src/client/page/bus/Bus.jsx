c5c2f90f8ea27f1dcfb6aa83e0617e127194ee01
XiongQi <xiongqi.me@foxmail.com>
2018-05-17 19:44:35 +0800

    Feat: school bus

--- src/client/page/bus/Bus.jsx
+++ src/client/page/bus/Bus.jsx
@@ -1,21 +1,42 @@
 import React from 'react';
-import { Table } from 'antd';
+import { Table, message } from 'antd';
 import ReactDOM from 'react-dom';
-import { fetchJSON } from '../../utils/fetchFunc';
-import { ApiBase } from '../../utils/common';
+import Header from 'components/Header';
+import { fetchJSON } from 'utils/fetchFunc';
+import { ApiBase } from 'utils/common';
 import './bus.scss';
 
 class Course extends React.Component {
   state = {
     bus: [],
-    message: [],
+    tips: [],
   };
   componentWillMount() {
     const that = this;
-    fetchJSON(`${ApiBase}/bus`).then(res => that.setState({ bus: res.data, message: res.message }));
+    const loaded = message.loading('获取校车信息 ...', 0);
+    fetchJSON(`${ApiBase}/bus`).then(res => that.setState({ bus: res.data, tips: res.tips }, loaded));
   }
+  componentDidMount() {
+    this.handleFixedTable();
+  }
+  handleFixedTable = () => {
+    const table = document.getElementById('fixedTable');
+    const width = document.documentElement.clientWidth < 768 ? 40 : 50;
+    window.onscroll = () => {
+      if (table) {
+        const { visibility } = table.style;
+        if (window.pageYOffset > width && visibility !== 'visible') {
+          table.style.visibility = 'visible';
+          console.log('visible...');
+        } else if (window.pageYOffset <= width && visibility !== 'hidden') {
+          console.log(visibility, 'hidden...');
+          table.style.visibility = 'hidden';
+        }
+      }
+    };
+  };
   render() {
-    const { bus, message } = this.state;
+    const { bus, tips } = this.state;
     const columnsConf = [
       {
         title: (bus[0] && bus[0].busCol1) || '',
@@ -23,7 +44,7 @@ class Course extends React.Component {
         width: 100,
         render: (text, record, index) => {
           if (index === 0) return { children: '1', props: { rowSpan: 0 } };
-          return text;
+          return <span>{text}</span>;
         },
       },
       {
@@ -32,7 +53,7 @@ class Course extends React.Component {
         width: 100,
         render: (text, record, index) => {
           if (index === 0) return { children: '1', props: { rowSpan: 0 } };
-          return text;
+          return <span>{text}</span>;
         },
       },
       {
@@ -41,7 +62,7 @@ class Course extends React.Component {
         width: 100,
         render: (text, record, index) => {
           if (index === 0) return { children: '1', props: { rowSpan: 0 } };
-          return text;
+          return <span>{text}</span>;
         },
       },
       {
@@ -50,7 +71,7 @@ class Course extends React.Component {
         width: 100,
         render: (text, record, index) => {
           if (index === 0) return { children: '1', props: { rowSpan: 0 } };
-          return text;
+          return <span>{text}</span>;
         },
       },
     ];
@@ -59,13 +80,36 @@ class Course extends React.Component {
       bordered: true,
       columns: columnsConf,
       pagination: false,
-      // showHeader: false,
+      rowKey: (v, k) => k,
+    };
+    const headerConf = {
+      left: '校车信息',
+      color: '#1cc769',
     };
     return (
-      <div className="container">
-        <Table {...tableConf} />
-        <div className="message">
-          {message.map(x => <p>{x}</p>)}
+      <div>
+        <Header {...headerConf} />
+        <div className="container">
+          <table id="fixedTable">
+            <thead>
+              <tr>
+                <th>
+                  <span>{bus[0] && bus[0].busCol1}</span>
+                </th>
+                <th>
+                  <span>{bus[0] && bus[0].busCol2}</span>
+                </th>
+                <th>
+                  <span>{bus[0] && bus[0].busCol3}</span>
+                </th>
+                <th>
+                  <span>{bus[0] && bus[0].busCol4}</span>
+                </th>
+              </tr>
+            </thead>
+          </table>
+          <Table {...tableConf} />
+          <div className="tips">{tips.map(x => <p>{x}</p>)}</div>
         </div>
       </div>
     );
