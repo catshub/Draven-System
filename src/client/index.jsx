e389f929474a36cbeace48cc8143fde549e76b61
XiongQi <xiongqi.me@foxmail.com>
2018-05-18 11:14:01 +0800

    Fix: adjust index <button> to <a>

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -5,21 +5,25 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 
 class Home extends React.Component {
+  state = {
+    login: false,
+  };
   componentWillMount() {
-    if (!document.cookie || !sessionStorage.getItem('zjh') || !sessionStorage.getItem('name')) {
-      window.location.href = `${window.location.origin}/draven/login`;
+    if (document.cookie && sessionStorage.getItem('zjh') && sessionStorage.getItem('name')) {
+      this.setState({ login: true });
     }
   }
-  handleClick = target => {
-    window.location.href = `${window.location.origin}/draven/${target}`;
-  };
+  // handleClick = target => {
+  //   window.location.href = `${window.location.origin}/draven/${target}`;
+  // };
   render() {
     const { Content } = Layout;
+    const { login } = this.state;
     const headerConf = {
-      title: sessionStorage.name,
-      subTitle: sessionStorage.zjh,
+      middle: <div className="name">{(login && sessionStorage.name) || '游客'}</div>,
+      right: <a href="/draven/login">{login ? '注销' : '登录'}</a>,
     };
-    const rowConf = { type: 'flex', justify: 'space-around', style: { padding: '10px 0' } };
+    const rowConf = { type: 'flex', justify: 'space-around', className: 'functionrow' };
     const colConf = { span: 7, className: 'col' };
     return (
       <Layout>
@@ -28,37 +32,52 @@ class Home extends React.Component {
           <Row {...rowConf}>
             <Divider>常用功能</Divider>
             <Col {...colConf}>
-              <Button onClick={() => this.handleClick('grade')} className="button-grade">
-                <span>📋</span>成绩
-              </Button>
+              <a href="/draven/grade">
+                <Button /* onClick={() => this.handleClick('grade')} */ disabled={!login} className="button-grade">
+                  <span>📋</span>成绩
+                </Button>
+              </a>
             </Col>
             <Col {...colConf}>
-              <Button onClick={() => this.handleClick('classroom')} className="button-classroom">
-                <span>️🏫</span>教室
-              </Button>
+              <a href="/draven/course">
+                <Button /* onClick={() => this.handleClick('course')}  */ disabled={!login} className="button-course">
+                  <span>📚</span>课程
+                </Button>
+              </a>
             </Col>
             <Col {...colConf}>
-              <Button onClick={() => this.handleClick('course')} className="button-course">
-                <span>📚</span>课程
-              </Button>
+              <a href="/draven/classroom">
+                <Button /* onClick={() => this.handleClick('classroom')} */ className="button-classroom">
+                  <span>️🏫</span>教室
+                </Button>
+              </a>
             </Col>
           </Row>
           <Row {...rowConf}>
             <Divider>其他功能</Divider>
             <Col {...colConf}>
-              <Button onClick={() => this.handleClick('takecourse')} className="button-takecourse">
-                <span>☁️</span>抢课
-              </Button>
+              <a href="/draven/takecourse">
+                <Button
+                  /* onClick={() => this.handleClick('takecourse')} */ disabled={!login}
+                  className="button-takecourse"
+                >
+                  <span>☁️</span>抢课
+                </Button>
+              </a>
             </Col>
             <Col {...colConf}>
-              <Button onClick={() => this.handleClick('bus')} className="button-bus">
-                <span>🚌</span>校车
-              </Button>
+              <a href="/draven/bus">
+                <Button /*  onClick={() => this.handleClick('bus')} */ className="button-bus">
+                  <span>🚌</span>校车
+                </Button>
+              </a>
             </Col>
             <Col {...colConf}>
-              <Button onClick={() => this.handleClick('calendar')} className="button-calendar">
-                <span>📅</span>校历
-              </Button>
+              <a href="/draven/calendar">
+                <Button /* onClick={() => this.handleClick('calendar')} */ className="button-calendar">
+                  <span>📅</span>校历
+                </Button>
+              </a>
             </Col>
           </Row>
         </Content>
