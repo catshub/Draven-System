8959dd06087f91c775836282e2603d5791a7456c
XiongQi <xiongqi.me@foxmail.com>
2018-05-19 09:50:56 +0800

    perf: adjust Header

--- src/client/components/Header/index.jsx
+++ src/client/components/Header/index.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
-// import {  } from 'antd';
+import { Row, Col } from 'antd';
 import './index.scss';
 
 export default class Header extends React.Component {
-  handleClick = target => {
-  };
   render() {
-    const { title, subTitle } = this.props;
+    const {
+      left = '', middle = '', right = '', color = '#f13b38'
+    } = this.props;
+    const rowConf = {
+      justify: 'space-between',
+    };
     return (
-      <div className="header">
-        <strong>{title}</strong>
-        <span>{subTitle}</span>
+      <div className="header" style={{ backgroundColor: color }}>
+        <Row {...rowConf}>
+          <Col md={6} xs={6} className="left">
+            {left}
+          </Col>
+          <Col md={12} xs={12} className="middle">
+            {middle}
+          </Col>
+          <Col md={6} xs={6} className="right">
+            {right || (
+              <a href="/draven" className="home">
+                主页
+              </a>
+            )}
+          </Col>
+        </Row>
       </div>
     );
   }
